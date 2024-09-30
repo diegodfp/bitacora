f15ea8c390bd1fa206599a62dba895d523d8009d
diegodfp <diegodfp.fly@gmail.com>
2024-09-30 11:09:44 -0500

    cors

--- bitacora/src/main/java/com/diavlo/bitacora/security/WebConfig.java
+++ bitacora/src/main/java/com/diavlo/bitacora/security/WebConfig.java
@@ -1,18 +1,21 @@
 package com.diavlo.bitacora.security;
 
-
+import org.springframework.lang.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig  implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Permite CORS para todas las rutas
-                .allowedOrigins("http://localhost:4200") // Cambia esto si tu frontend está en otro dominio
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500", "https://sp32frontend.onrender.com/","https://cheery-dasik-60d356.netlify.app/") // Especifica tus orígenes
+                                                                                             // permitidos
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
 }
